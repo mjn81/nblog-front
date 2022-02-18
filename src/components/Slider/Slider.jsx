5135afb21741f48090b73bc254afca3875aae145
mjn81 <mjjnn81@gmail.com>
2022-02-18 17:01:26 +0330

    Slider responsive implemented

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,22 +1,25 @@
 import {useEffect} from "react";
-import {OutlineButton} from "../core/Buttton";
+import {OutlineButton} from "../core";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
 
 const Slider = () => {
     const images = [{
-        title: 'new newss', img: 'https://picsum.photos/1000/300'
+        title: 'new newss', img: 'https://via.placeholder.com/1920x1080'
     }, {
-        title: 'new newss', img: 'https://picsum.photos/1000/300'
+        title: 'new newss', img: 'https://via.placeholder.com/1920x1080'
     }, {
-        title: 'new newss', img: 'https://picsum.photos/1000/300'
+        title: 'new newss', img: 'https://via.placeholder.com/1920x1080'
     }, {
-        title: 'new newss', img: 'https://picsum.photos/200/300'
+        title: 'new newss', img: 'https://via.placeholder.com/1920x1080'
     }, {
-        title: 'new newss', img: 'https://picsum.photos/200/500'
+        title: 'new newss', img: 'https://via.placeholder.com/1920x1080'
     },
 
     ]
+
+    //TODO: ADD IMPROVEMENTS
+
     const carouselFunction = () => {
         const track = document.querySelector(".main-slider");
         const slides = track.querySelectorAll("img");
@@ -41,21 +44,21 @@ const Slider = () => {
         return [track, current];
     }
     const moveToNext = () => {
-        const [track , current] = getElements();
+        const [track, current] = getElements();
         const next = current.nextElementSibling;
-        moveToSlide(track , current , next);
+        moveToSlide(track, current, next);
     }
     const moveToPrev = () => {
-        const [track , current] = getElements();
+        const [track, current] = getElements();
         const prev = current.previousElementSibling;
-        moveToSlide(track , current , prev);
+        moveToSlide(track, current, prev);
     }
 
     const autoMovement = () => {
-        const [track , current] = getElements();
+        const [track, current] = getElements();
         const next = current.nextElementSibling;
-        moveToSlide(track , current , next);
-        if (!next){
+        moveToSlide(track, current, next);
+        if (!next) {
             return;
         }
         setTimeout(autoMovement, 10000);
@@ -64,23 +67,27 @@ const Slider = () => {
 
     useEffect(() => {
         carouselFunction();
-        setTimeout(autoMovement , 10000);
+        window.addEventListener('resize',carouselFunction );
+        const clr = setTimeout(autoMovement, 10000);
+        return () => {
+            clearTimeout(clr);
+        }
     });
 
+    return (<section className=" my-8 overflow-hidden rounded-xl relative text-white h-500">
+        <div className="main-slider relative transition-transform duration-500 ease-out h-full">
+            {images.map(({img, title}, index) => (
+                <img key={index} className={"w-full h-full object-cover absolute" + (index === 0 ? ' active' : '')}
+                     src={img} alt={title}/>))}
+        </div>
+        <OutlineButton onClick={moveToPrev} adclass='absolute z-10 top-1/2 left-5 -translate-y-1/2 py-3 px-2'>
+            <FontAwesomeIcon icon={faChevronLeft} className="text-4xl"/>
+        </OutlineButton>
+        <OutlineButton onClick={moveToNext} adclass='absolute z-10 top-1/2 right-5 -translate-y-1/2 py-3 px-2'>
+            <FontAwesomeIcon icon={faChevronRight} className="text-4xl"/>
+        </OutlineButton>
 
-    return (<section className=" my-6 overflow-hidden rounded-xl h-96 relative">
-            <div className="main-slider relative transition-transform duration-500 ease-out h-full">
-                {images.map(({img, title}, index) => (
-                    <img key={index} className={"w-full h-full object-cover absolute" +(index===0 ? ' active' : '')} src={img} alt={title}/>))}
-            </div>
-            <OutlineButton onClick={moveToPrev} adclass='absolute z-10 top-1/2 left-5 -translate-y-1/2 py-3 px-2'>
-                <FontAwesomeIcon icon={faChevronLeft} className="text-4xl" />
-            </OutlineButton>
-            <OutlineButton onClick={moveToNext} adclass='absolute z-10 top-1/2 right-5 -translate-y-1/2 py-3 px-2'>
-                <FontAwesomeIcon icon={faChevronRight} className="text-4xl" />
-            </OutlineButton>
-
-        </section>);
+    </section>);
 }
 
 export default Slider;
